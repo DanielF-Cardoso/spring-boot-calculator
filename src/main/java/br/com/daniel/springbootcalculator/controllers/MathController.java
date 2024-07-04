35929182efb792561430e512d27a9e6c1252cac1
Daniel Fernandes <danielfca122@gmail.com>
2024-07-04 10:44:06 -0300

    Added operations: multiplication, division, mean, and square root

--- src/main/java/br/com/daniel/springbootcalculator/controllers/MathController.java
+++ src/main/java/br/com/daniel/springbootcalculator/controllers/MathController.java
@@ -28,4 +28,47 @@ public class MathController {
         Double subtraction = numberController.convertToDouble(numberOne) -  numberController.convertToDouble(numberTwo);
         return subtraction;
     }
+
+    @RequestMapping(value = "/multiplication/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+    public Double multiplication(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
+        NumberController numberController = new NumberController();
+        if (!numberController.isNumeric(numberOne) || !numberController.isNumeric(numberTwo)) {
+            throw new Exception();
+        }
+        Double multiplication = numberController.convertToDouble(numberOne) *  numberController.convertToDouble(numberTwo);
+        return multiplication;
+    }
+
+    @RequestMapping(value = "/division/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+    public Double division(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
+        NumberController numberController = new NumberController();
+        if (!numberController.isNumeric(numberOne) || !numberController.isNumeric(numberTwo)) {
+            throw new Exception();
+        }
+        if (numberController.convertToDouble(numberTwo) == 0) {
+            throw new Exception();
+        }
+        Double division = numberController.convertToDouble(numberOne) /  numberController.convertToDouble(numberTwo);
+        return division;
+    }
+
+    @RequestMapping(value = "/mean/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+    public Double mean(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
+        NumberController numberController = new NumberController();
+        if (!numberController.isNumeric(numberOne) || !numberController.isNumeric(numberTwo)) {
+            throw new Exception();
+        }
+        Double mean = (numberController.convertToDouble(numberOne) +  numberController.convertToDouble(numberTwo)) / 2;
+        return mean;
+    }
+
+    @RequestMapping(value = "/squareRoot/{number}", method = RequestMethod.GET)
+    public Double squareRoot(@PathVariable("number") String number) throws Exception {
+        NumberController numberController = new NumberController();
+        if (!numberController.isNumeric(number)) {
+            throw new Exception();
+        }
+        Double squareRoot = Math.sqrt(numberController.convertToDouble(number));
+        return squareRoot;
+    }
 }
